0ff85a1f9a1951355052bb4457d7ed6737514529
luizfernandoliveira <luiz.fernando_1990@hotmail.com>
2020-12-23 10:13:27 -0300

    Base do programa, inserido liga/desliga, inserido controle de volume e mudança de faixa.

---
+++ teste.py
@@ -0,0 +1,71 @@
+from playsound import playsound
+lista = ['ab', 'cd', 'ef', 'gh', 'jack_johnson.mp3']
+a = len(lista)
+print(a)
+print(lista[-1])
+print()
+print(len(lista[-1]))
+playsound(lista[a-1])
+
+
+from playsound import playsound
+radio_status = False
+volume = 5
+musica = 0
+lista_musica = ['Engenheiros do Hawai', 'Legião Urbana', 'Jota Quest', 'The Strokes', 'jack_johnson.mp3']
+while True:
+    if radio_status == False:
+        f = input('Rádio Desligado: ')
+        if f == 'l':  # liga/desliga
+            if radio_status == False:
+                radio_status = True
+                print('Rádio ligado')
+    else:
+        while True:
+            f = input('Funções: ')
+            if f == 'l':  # liga/desliga
+                if radio_status == True:
+                    radio_status = False
+                    print('Rádio desligado2')
+                    break
+                else:
+                    radio_status = False
+                    print(radio_status)
+
+            elif f == '+':  # aumenta volume
+                if volume < 10:
+                    volume += 1
+                    print('Volume está em {}'.format(volume))
+                else:
+                    print('Volume máximo!')
+            elif f == '-':
+                if volume > 0:
+                    volume -= 1
+                    print('Volume está em {}'.format(volume))
+                else:
+                    print('Volume mínimo!')
+            elif f == '<':
+                if musica > 0:
+                    musica -= 1
+                    print('Música: {}'.format(musica + 1))
+                    print(lista_musica[musica])
+                    playsound(lista_musica)
+                else:
+                    total_lista = len(lista_musica)
+                    musica = total_lista - 1
+                    print('Música: {}'.format(musica + 1))
+                    print(lista_musica[musica])
+            elif f == '>':
+                total_lista = len(lista_musica)
+                if musica < (total_lista - 1):
+                    musica += 1
+                    print('Música: {}'.format(musica + 1))
+                    print(lista_musica[musica])
+                    input()
+                else:
+                    musica = 0
+                    print('Música: {}'.format(musica + 1))
+                    print(lista_musica[musica])
+
+
+
